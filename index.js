321c72397b37b31f94903a1adf6f39b7762a3025
cristhian2121 <ccdelgadop@gmail.com>
2021-02-17 22:21:48 -0500

    queries quit params

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const { buildSchema } = require('graphql')
+// const { buildSchema } = require('graphql')
+const { makeExecutableSchema } = require('graphql-tools')
 const app = require('express')()
 const { graphqlHTTP } = require('express-graphql')
 const { readFileSync } = require('fs')
@@ -9,12 +10,14 @@ const { join } = require('path')
 const resolvers = require('./lib/resolvers')
 
 // Create Schema
-const schema = buildSchema(
-  readFileSync(
+const typeDefs = readFileSync(
     join(__dirname, 'lib', 'schema.graphql'),
     'utf-8'
-  )
 )
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
 
 // // execute schema
 // graphql(schema, '{ age }', resolvers)
@@ -22,9 +25,9 @@ const schema = buildSchema(
 //     .catch( e => console.log(e))
 
 app.use('/api', graphqlHTTP({
-  schema: schema,
-  rootValue: resolvers,
-  graphiql: true
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
 }))
 
 app.listen(4000, () => console.log('Server start im http://localhost:4000/api'))
